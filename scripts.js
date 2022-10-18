a352c54423c2c0a069245fc8b8ad014bbd7006be
Robert Siciliano <Robert.siciliano22@gmail.com>
2022-10-18 11:09:59 -0400

    Basic display function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+// Get elements
+const bookForm = document.getElementById('bookForm');
+const addBookBtn = document.getElementById('addBookBtn');
+const books = document.querySelector('.books');
+
+
+
+
+
+// Array that will hold all of the Book objects created by the user
 let myLibrary = [];
 
 // Constructor function
@@ -6,19 +16,67 @@ function Book(title, author, pages) {
     this.author = author;
     this.pages = pages;
     this.read = false;
+};
+
+// Prototype that changes the read property
+Book.prototype.changeStatus = function() {
+    if (this.read == false) return this.read = true;
+    return this.read = false;
 }
-Book.prototype.addToMyLibrary = function() {
-    myLibrary.push(this);
-    console.log(myLibrary);
+
+function displayBook(myLibrary) {
+    // For each object in myLibrary create a new book card displaying its information
+    for (let i = 0; i < myLibrary.length; i++) {
+        let inputValues = (Object.values(myLibrary[i]));
+        console.log(inputValues);
+
+        // Card div that will display each book
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book');
+        // Information inside of card div
+        const bTitle = document.createElement('p');
+        const bAuthor = document.createElement('p');
+        const bPages = document.createElement('p');
+        const bRead = document.createElement('label');
+        
+        // Edit book card information values
+        bTitle.innerHTML = `Title: ${inputValues[0]}`;
+        bAuthor.innerHTML = `Author: ${inputValues[1]}`;
+        bPages.innerHTML = `Pages: ${inputValues[2]}`;
+        if (inputValues[3] == true) bRead.innerHTML = 'Read: <input type="checkbox" checked>';
+        else bRead.innerHTML = 'Read: <input type="checkbox">'
+        
+        // Append information to book card
+        bookCard.appendChild(bTitle);
+        bookCard.appendChild(bAuthor);
+        bookCard.appendChild(bPages);
+        bookCard.appendChild(bRead);
+
+        // Append book card to books div
+        return books.appendChild(bookCard);
+    }
+    return;
 }
 
-const book1 =  new Book("The 48 Laws of Power", "Robert Greene", 450);
-const book2 =  new Book("The 48", "Robert", 45);
-
-book1.addToMyLibrary();
-book2.addToMyLibrary();
-
-myLibrary.forEach(addBookToLibrary);
+addBookBtn.addEventListener('click', addBookToLibrary);
+// Takes user input and stores new book object into myLibrary array
 function addBookToLibrary() {
-    // Display book on web page
-}
+    // temporary array for form values
+    let valueArray = []; 
+    // Get values of each input field
+    const info = document.querySelectorAll('.info');
+    info.forEach((e) => {
+        if (e.type == 'checkbox') return valueArray.push(e.checked);
+        return valueArray.push(e.value);
+        // Create new Book object with these values
+    });
+    let book = new Book(valueArray[0], valueArray[1], valueArray[2]); 
+    // If read is checked, change book status to read
+    if (valueArray[3] == true) book.changeStatus();
+    // Add new book to myLibrary array and send that array to the display function
+    myLibrary.push(book);
+    return displayBook(myLibrary);
+};
+
+
+
